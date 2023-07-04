3876c74b08a0c3ad28cea25b65fcdce71b376194
Hammam Tawafshah <45073052+HammamT@users.noreply.github.com>
2023-07-04 22:55:46 +0300

    v2

--- src/main/java/com/orderManagement/example/Swagger/SpringdocConfig.java
+++ src/main/java/com/orderManagement/example/Swagger/SpringdocConfig.java
@@ -2,13 +2,22 @@ package com.orderManagement.example.Swagger;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition
 @Configuration
+@SecurityScheme(
+        name = "token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SpringdocConfig {
 
     @Bean
@@ -16,5 +25,24 @@ public class SpringdocConfig {
 
         return new OpenAPI()
                 .info(new Info().title("Spring Doc").version("1.0.0").description("Spring doc"));
+    }
+
+
+    @Bean
+    public GroupedOpenApi authenticationApi() {
+        String[] paths = {"/auth/**"};
+        return GroupedOpenApi.builder()
+                .group("Authentication")
+                .pathsToMatch(paths)
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi postApi() {
+        String[] paths = {"/post/**"};
+        return GroupedOpenApi.builder()
+                .group("Post")
+                .pathsToMatch(paths)
+                .build();
     }
 }
